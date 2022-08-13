0915f84bbaa55c07e92fba4c29b65c083c4f9ce9
Mohdaman01 <mohdastar200026@gmail.com>
2022-08-13 18:19:01 +0530

    committed

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import { HiOutlineMenu,  HiXCircle} from 'react-icons/hi';
+
+import { motion } from 'framer-motion';
+
+import './Navbar.scss';
+
+import { images } from '../../constants';
+
+
+// import ends // 
+
+const Navbar = () => {
+   const [toggle, setToggle] = useState(false)
+
+
+  return (
+     <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <h1>PORTFOLIO</h1>
+        {/* <img src={images.logo} alt="logo" /> */}
+      </div>
+      <ul className='app__navbar-links'>
+        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li className='app__flex p-text' key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}</ul>
+
+        <div className='app__navbar-manu'>
+            < HiOutlineMenu onClick={()=> setToggle(true)}/>
+
+            {toggle && (
+              <motion.div
+                whileInView={{ x:[300,0]}}
+                transition={{ duration:0.85, ease: 'easeOut'}}
+                >
+                  < HiXCircle onClick={()=>setToggle(false)} />
+                  <ul>
+                  {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+          <li key={item}>
+             
+            <a href={`#${item}`} onClick={()=>setToggle(false)}>{item}</a>
+          </li>
+           
+        ))}
+        </ul>
+              </motion.div>
+              )}
+        </div>
+
+     </nav>
+  )
+}
+
+export default Navbar
---
+++ src/container/About/About.jsx
@@ -0,0 +1,21 @@
+import React,{ useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+
+import './About.scss';
+
+const About = () => {
+  return (
+     <>
+     <h1 className="head-text">
+      I Know That 
+      <span>Good Design</span>
+      <br />
+      means
+      <span>Good Business</span>
+     </h1>
+     </>
+  )
+}
+
+export default About
